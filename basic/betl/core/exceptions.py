f6c604c29faca049011687d721fc11d3e6c5334b
Britain Cox <britaincox@Alexander.local>
2020-02-08 14:31:19 -0600

    More betl

--- basic/betl/core/exceptions.py
+++ basic/betl/core/exceptions.py
@@ -1,6 +1,16 @@
 
 
-class ConfigurationException(Exception):
+class BetlError(Exception):
     def __init__(self, *a, **k):
         self.k = k
-        super(ConfigurationException).__init__(self, *a)
+        super(BetlError).__init__(self, *a)
+
+
+class ConfigurationError(BetlError):
+    def __init__(self, *a, **k):
+        super(ConfigurationError).__init__(self, *a, **k)
+
+
+class DataError(BetlError):
+    def __init__(self, *a, **k):
+        super(DataError).__init__(self, *a, **k)
